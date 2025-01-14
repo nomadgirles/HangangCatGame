a500fd85c9f9fd5f7971e7b0279d41755a0a69ee
nomadgirles <nomadgirles@gmail.com>
2025-01-14 17:17:37 +0900

    한강 고양이 게임 만들기

---
+++ README.md
@@ -0,0 +1,46 @@
+# 🐈 꽁꽁 얼어붙은 한강 위를 걸어다니는 고양이 게임
+
+## 링크
+
+- 배포 링크: [고양이 게임 시작하기](https://nomadgirles.github.io/HangangCatGame/)
+- 튜토리얼 영상: [아파트 게임 만들기](https://www.youtube.com)
+
+## 기능 설명
+
+1. **게임 시작**: START 버튼을 누릅니다.
+2. **고양이 점프**: `Space` 또는 `⬆`를 눌러 장애물을 피합니다.
+
+## 폴더 구조
+
+```
+📂 HangangCatGame
+
+├── 📂 img # 이미지 파일 디렉토리
+├── 📄 index.html # 메인 HTML 파일
+├── 📄 script.js # 게임 로직 및 Canvas JavaScript 파일
+└── 📄 styles.css # 스타일 CSS 파일
+```
+
+- img 폴더 이미지 파일 : [구글 드라이브 다운받기](https://drive.google.com/drive/folders/1PM794aitofkcbZIQ1I5x5qN2nWAcx1Wj?usp=drive_link)
+
+## 기술 스택
+
+[![Skills](https://skillicons.dev/icons?i=html,css,js)](https://skillicons.dev)
+
+- `HTML5`: 구조와 콘텐츠 마크업
+- `CSS3`: 레이아웃과 스타일링
+- `JavaScript`: 게임 로직 및 동적 기능 구현
+
+## 다운로드 및 실행 방법
+
+1. 해당 저장소를 로컬 환경으로 클론
+
+   ```bash
+   git clone https://github.com/nomadgirles/HangangCatGame.git
+   ```
+
+2. HTML 파일을 브라우저에서 열어서 실행
+
+## 라이선스
+
+이 프로젝트는 MIT 라이선스를 따릅니다.
---
+++ script.js
@@ -0,0 +1,241 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+// 게임 시작 버튼 눌렀을 때의 이벤트
+document.querySelector(".game-start").addEventListener("click", () => {
+  const gameTutorial = document.querySelector(".game-tutorial");
+  canvas.width = gameTutorial.clientWidth;
+  canvas.height = 400;
+  gameTutorial.style.display = "none";
+
+  const gameStart = document.querySelector(".game-start");
+  gameStart.style.display = "none";
+
+  const score = document.querySelector(".game-score");
+  score.style.display = "flex";
+
+  frameRun();
+});
+
+// 움직이는 배경 만들기
+let backgroundX = 0;
+const backgroundImage = new Image();
+backgroundImage.src = "./img/background.png";
+
+function drawBackground() {
+  ctx.drawImage(backgroundImage, backgroundX, 0, canvas.width, 400);
+  ctx.drawImage(
+    backgroundImage,
+    backgroundX + canvas.width,
+    0,
+    canvas.width,
+    400
+  );
+
+  backgroundX -= 2;
+  if (backgroundX <= -canvas.width) {
+    backgroundX = 0;
+  }
+}
+
+let timer = 0;
+let animation;
+
+function frameRun() {
+  // 움직이는 배경
+  animation = requestAnimationFrame(frameRun);
+  ctx.clearRect(0, 0, canvas.widht, canvas.height);
+
+  drawBackground();
+  timer++;
+
+  // 움직이는 바닥
+  drawFloor();
+
+  // 장애물
+  if (Math.random() < 0.01) {
+    let obstacleNum = Math.round(Math.random() * 2);
+    let obstacle = new Obstacle(obstacleNum);
+
+    manyObstacles.push(obstacle);
+  }
+  manyObstacles.forEach((a, i, o) => {
+    if (a.x < 0) o.splice(i, 1);
+    a.x = a.x - 5;
+
+    a.draw();
+
+    crash(cat, a); // 고양이와 장애물 충돌 감지
+  });
+
+  // 고양이
+  if (timer % 20 === 0) {
+    currentCat = (currentCat + 1) % 2;
+  }
+  cat.draw();
+
+  // 고양이 점프
+  if (jumpSwitch) {
+    cat.jump();
+    jumpTimer++;
+  } else {
+    if (cat.y < 270) {
+      if (cat.y == 180) jumpCount = 1;
+      else if (cat.y == 260) jumpCount = 0;
+      cat.y = cat.y + 2;
+    }
+  }
+  if (jumpTimer > 8) {
+    jumpSwitch = false;
+    jumpTimer = 0;
+  }
+}
+
+// 움직이는 바닥 만들기
+let floorX = 0;
+const floorImage = new Image();
+floorImage.src = "./img/floor.png";
+function drawFloor() {
+  ctx.drawImage(floorImage, floorX, 320, canvas.width, 100);
+  ctx.drawImage(floorImage, floorX + canvas.width, 320, canvas.width, 100);
+
+  floorX -= 4;
+  if (floorX <= -canvas.width) {
+    floorX = 0;
+  }
+}
+
+// 랜덤 장애물 그리기
+let manyObstacles = [];
+const obstacleImgs = [
+  { img: new Image(), y: 286, width: 40, height: 45 },
+  { img: new Image(), y: 286, width: 40, height: 45 },
+  { img: new Image(), y: 320, width: 70, height: 80 },
+];
+obstacleImgs[0].img.src = "./img/obstacle1.png";
+obstacleImgs[1].img.src = "./img/obstacle2.png";
+obstacleImgs[2].img.src = "./img/obstacle3.png";
+
+class Obstacle {
+  constructor(num) {
+    this.num = num;
+    this.width = obstacleImgs[num].width;
+    this.height = obstacleImgs[num].height;
+    this.x = canvas.width - this.width;
+    this.y = obstacleImgs[num].y;
+  }
+  draw() {
+    ctx.drawImage(
+      obstacleImgs[this.num].img,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+// 점수 기록
+let score = 0;
+let scoreInterval = setInterval(updateScore, 1000);
+function updateScore() {
+  score += 1;
+  document.querySelector(".game-score span").textContent = score;
+}
+
+// 움직이는 고양이 그리기
+const catImages = [new Image(), new Image()];
+catImages[0].src = "./img/cat1.png";
+catImages[1].src = "./img/cat2.png";
+
+let currentCat = 0;
+let cat = {
+  x: 30,
+  y: 270,
+  width: 70,
+  height: 60,
+  draw() {
+    ctx.drawImage(
+      catImages[currentCat],
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  },
+  jump() {
+    this.y -= 12;
+  },
+  fall() {
+    if (this.y < 10) {
+      this.y += 8;
+    }
+  },
+};
+
+// 고양이와 장애물 충돌
+
+function crash(cat, obstacle) {
+  let xCal = obstacle.x - (cat.x + cat.width);
+  let yCal = obstacle.y - (cat.y + cat.height);
+
+  if (
+    (xCal < 0 && yCal < 0) ||
+    (xCal < 0 && obstacle.num == 2 && cat.y == 270)
+  ) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    cancelAnimationFrame(animation);
+    clearInterval(scoreInterval);
+    const totalScore = document.querySelector(".total-score");
+    const gameOver = document.querySelector(".game-over");
+
+    totalScore.textContent = `${score}`;
+    gameOver.style.display = "flex";
+
+    manyObstacles = [];
+
+    const gameTutorial = document.querySelector(".game-tutorial");
+    gameTutorial.style.display = "block";
+  }
+}
+
+// 고양이 점프
+var jumpSwitch = false;
+let jumpCount = 0;
+let jumpTimer = 0;
+let lastSpacePressTIme = 0;
+
+document.addEventListener("keydown", function (e) {
+  if (e.code === "Space" || e.code === "ArrowUp") {
+    if (jumpCount <= 1) {
+      jumpSwitch = true;
+      jumpCount++;
+    }
+  }
+});
+
+// 리플레이
+document.querySelector(".replay").addEventListener("click", () => {
+  replayGame();
+});
+
+function replayGame() {
+  cancelAnimationFrame(animation);
+  clearInterval(scoreInterval);
+  score = 0;
+  document.querySelector(".game-score span").textContent = score;
+  manyObstacles = [];
+  currentCat = 0;
+  jumpSwitch = false;
+  lastSpacePressTIme = 0;
+
+  frameRun();
+
+  const gameOver = document.querySelector(".game-over");
+  gameOver.style.display = "none";
+  const gameTutorial = document.querySelector(".game-tutorial");
+  gameTutorial.style.display = "none";
+
+  if (scoreInterval) clearInterval(scoreInterval);
+  scoreInterval = setInterval(updateScore, 1000);
+}
